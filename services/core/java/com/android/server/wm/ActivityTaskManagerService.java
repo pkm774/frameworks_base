b7ea3b2d68c4aa5a7a32aac0f4efc38dc2790af0
pkm774 <mprabhat774@gmail.com>
2024-05-13 07:48:10 +0530

    Drop PropImitationHooks
    Revert "PropImitationHooks: Update fingerprints for May release"
    
    This reverts commit 87bca3b5bdf72a869b0a26917ef18c06947c4a2f.
    
    Revert "PropImitationHooks: Update fingerprints for April release"
    
    This reverts commit 11b6b3da9280304423499e458b04769639ef9e63.
    
    Revert "PIHooks: Update fingerprints from March release"
    
    This reverts commit df5e38a62cb976f6ba365ef3ebe06c14ac1fd4b6.
    
    Revert "PIHooks: Spoof as Pixel Tab for tablets"
    
    This reverts commit 56cbd529211e180c099415847aeb615a05facedd.
    
    Revert "PIHooks: Spoof more Google apps"
    
    This reverts commit f2d2ebe01540acdc4bcea0b9c760163159fabf70.
    
    Revert "PIHooks: Spoof Pixel 8 Pro to some Google apps"
    
    This reverts commit f2a726c4057d92b4907940ee107db92f59e3f9f9.
    
    Revert "PIHooks: Set system props for native hook [2/2]"
    
    This reverts commit 8f0e84bb11f8ba8c04c9fb245bfcba934d5b07cd.
    
    Revert "PIHooks: Get resources from context"
    
    This reverts commit 8c53c13570fde8d9a97709cda35503e32962216c.
    
    Revert "PIHooks: Add a system prop to enable debug logs"
    
    This reverts commit 63fcf78bb57e4d03b7f9518209cac28d1df8bbb5.
    
    Revert "PIHooks: Spoof more gapps and google photos"
    
    This reverts commit 563b30cd553fe3932ec133c7c0fb56ce7b575931.
    
    Revert "PIHooks: Allow spoofing device model for Netflix"
    
    This reverts commit 7197bbdb51a2a13affddd4cd296bfc9deae0f496.
    
    Revert "PIHooks: Allow setting any field in certified build props"
    
    This reverts commit 9ef01c9261e1b146e58cc7a9f95e393d0e532431.
    
    Revert "PIHooks: Un-spoof GMS while adding google account"
    
    This reverts commit 7790460563a00dee8cc0abea487a5e58e98e02c9.
    
    Revert "PIHooks: Get package name from context instead of app"
    
    This reverts commit a958a380272d3616673da6310d0aa9d33f3df4d5.
    
    Revert "PIHooks: Alter model name to avoid SafetyNet HW attestation"
    
    This reverts commit 5304030da1e2a7a77d558443ec735434448a605e.
    
    Revert "Introduce PropImitationHooks"
    
    This reverts commit 9611e0d9ad100e2c6a1f177ed146bdfbd043fd57.
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -286,7 +286,6 @@ import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 import com.android.server.usage.AppStandbyInternal;
 
-import com.android.internal.util.PropImitationHooks;
 import com.android.internal.util.custom.cutout.CutoutFullscreenController;
 
 import java.io.BufferedReader;
@@ -1992,9 +1991,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("getFocusedRootTaskInfo()");
-        }
+        enforceTaskPermission("getFocusedRootTaskInfo()");
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3113,9 +3110,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("registerTaskStackListener()");
-        }
+        enforceTaskPermission("registerTaskStackListener()");
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
